6c7d72c3463a3b8e792b92ac653d7d77bb75ecad
gitgut0110 <jsimms0110@gmail.com>
2018-11-02 21:34:38 +0100

    Updates

---
+++ precache-manifest.53b213d9b2ad3cae20784baf9c958ccc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/challenge-signup-multipage/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4c1d748cb81278796dd3",
+    "url": "/challenge-signup-multipage/static/js/runtime~main.4c1d748c.js"
+  },
+  {
+    "revision": "e26829b98d9a4d8312b1",
+    "url": "/challenge-signup-multipage/static/js/main.e26829b9.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/challenge-signup-multipage/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "e26829b98d9a4d8312b1",
+    "url": "/challenge-signup-multipage/static/css/main.44f627f3.chunk.css"
+  },
+  {
+    "revision": "9dd7c98350d7768ec56b0080348cf3ce",
+    "url": "/challenge-signup-multipage/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/challenge-signup-multipage/precache-manifest.53b213d9b2ad3cae20784baf9c958ccc.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/challenge-signup-multipage/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
